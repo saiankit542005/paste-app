70b67ed54b7d204ba19d5c075e76eea41070bf53
Ankit Saini <saiankit542005@gmail.com>
2026-06-14 03:23:12 +0530

    Add button in ViewPaste

--- src/components/ViewPaste.jsx
+++ src/components/ViewPaste.jsx
@@ -1,16 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 
 const ViewPaste = () => {
   const { id } = useParams();
   const allPastes = useSelector((state) => state.paste.pastes);
   const paste = allPastes.filter((p) => p._id === id)[0];
   console.log("Final Paste : ", paste);
+  const navigate = useNavigate();
 
   return (
     <div className="flex flex-col items-center mt-12 px-4">
       {/* Title */}
-      <div className="w-full max-w-3xl mb-6">
+      <div className="flex w-full max-w-3xl mb-6">
         <input
           type="text"
           value={paste.title}
@@ -18,6 +19,7 @@ const ViewPaste = () => {
           className="w-full px-5 py-3 rounded-2xl border border-gray-300 bg-gray-100 text-gray-700 font-semibold shadow-sm focus:outline-none cursor-not-allowed"
           placeholder="Title"
         />
+        <button onClick={()=>navigate("/pastes")} className="bg-blue-500 text-white px-8 py-2 rounded-lg hover:bg-blue-600 shadow-md transition ml-4">Pastes</button>
       </div>
 
       {/* Content */}
